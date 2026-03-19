89d3d6c838f0e091a35c5fc393743635d81caafa
Software Tester <softwaretester@medphys038-mac-2.local>
2026-03-19 11:04:16 +0000

    Added function to get the median of a list

---
+++ functions/list_manipulations/get_median.py
@@ -0,0 +1,9 @@
+def get_median(lst):
+    sorted_lst = sorted(lst)
+    n = len(sorted_lst)
+    mid = n // 2
+
+    if n % 2 == 0:
+        return (sorted_lst[mid - 1] + sorted_lst[mid]) / 2
+    else:
+        return sorted_lst[mid]
