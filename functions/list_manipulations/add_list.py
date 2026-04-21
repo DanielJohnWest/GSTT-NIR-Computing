21cfb8104963e8c7b12a6611748a069ae2b772ba
Software Tester <softwaretester@medphys038-mac-2.local>
2026-04-21 11:54:57 +0100

    I made a file that adds a list

---
+++ functions/list_manipulations/add_list.py
@@ -0,0 +1,5 @@
+def add_list(list1):
+    a = 0
+    for i in list1:
+        a = a+ i
+    return a
---
+++ functions/tests/test_add_list.py
@@ -0,0 +1,4 @@
+from functions.list_manipulations.add_list import add_list
+
+def test_add_list():
+    assert add_list([1,2]) == 3
